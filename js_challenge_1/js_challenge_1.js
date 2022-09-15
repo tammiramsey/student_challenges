702a03ea012175796262871c5ce0b2a320448253
tammiramsey <tammirice@gmail.com>
2022-09-15 09:03:50 -0400

    created JS challenge 1

---
+++ js_challenge_1/js_challenge_1.js
@@ -0,0 +1,11 @@
+//ask user their name
+let firstName=prompt ("What is your name?");
+
+// add second function here 
+
+// update the alert 
+alert("Hello " + firstName);
+
+// ask user how old they are & set age rules
+
+
