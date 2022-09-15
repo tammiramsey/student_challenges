e0d4769801ca3bac20155b1143e1883e501426cd
tammiramsey <tammirice@gmail.com>
2022-09-15 13:23:38 -0400

    updating JS challenge 2

--- js_challenge_1/js_challenge_1.js
+++ js_challenge_1/js_challenge_1.js
@@ -6,6 +6,6 @@ let firstName=prompt ("What is your name?");
 // update the alert 
 alert("Hello " + firstName);
 
-// ask user how old they are & set age rules
+// ask user how old they are & set age rules (hint: use an if/else statement here)
 
 
