6dc3c6f7635672a45c9ee87d34ff5a9c01693eb0
tammiramsey <tammirice@gmail.com>
2023-02-03 08:55:39 -0500

    finished js challenge instructions on index.js

---
+++ js_challenge_6/src/index.js
@@ -0,0 +1,14 @@
+// Challenge 1
+// Create an array called weekDays with all days of the week listed, console log it
+
+// Challenge 2
+// Log 'Monday' and 'Sunday'
+
+// Challenge 3
+// Change 'Sunday' to 'Funday' & log the array
+
+// Challenge 4
+// Remove Monday and Tuesday from the array
+
+// Challenge 5
+// Log every day of the week this way: `Temperate on Monday is 75 degrees'
