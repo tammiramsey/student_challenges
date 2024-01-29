8acf6d6760e3e8e7fcea42e07506e52b6a654143
Tammi Ramsey <97861522+tammiramsey@users.noreply.github.com>
2024-01-29 09:43:48 -0500

    Update index.js
    fixed spelling error

--- js_challenge_6/src/index.js
+++ js_challenge_6/src/index.js
@@ -11,4 +11,4 @@
 // Remove Monday and Tuesday from the array
 
 // Challenge 5
-// Log every day of the week this way: `Temperate on Monday is 75 degrees'
+// Log every day of the week this way: `Temperature on Monday is 75 degrees'
